1cdb622442a2f29e8d95522a9958b4d637506f28
firstclimbkazu <firstclimbkazu@gmail.com>
2021-06-22 14:09:24 +0900

    環境変数設定追加

--- external/database.go
+++ external/database.go
@@ -2,13 +2,22 @@ package external
 
 import (
 	"firstclimb-go/entity"
+	"os"
 
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDatabase() (*gorm.DB, error) {
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	DbUser := os.Getenv("DB_USER")
+	DbPass := os.Getenv("DB_PASS")
+	DbHost := os.Getenv("DB_HOST")
+	DbPort := os.Getenv("DB_PORT")
+	DbName := os.Getenv("DB_NAME")
+
+	dsn := DbUser + ":" + DbPass + "@tcp(" + DbHost + ":" + DbPort + ")/" + DbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
--- main.go
+++ main.go
@@ -4,17 +4,24 @@ import (
 	"firstclimb-go/external"
 	"firstclimb-go/graph"
 	"firstclimb-go/graph/generated"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/joho/godotenv"
 )
 
 const defaultPort = "5000"
 
 func main() {
+	err := godotenv.Load(fmt.Sprintf("./.%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		return
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
