32dd933ba0b1a04f5e8485b0093101394c7e5346
firstclimbkazu <firstclimbkazu@gmail.com>
2021-07-27 19:48:28 +0900

    gin導入

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/99designs/gqlgen v0.13.0
 	github.com/garyburd/redigo v1.6.2 // indirect
+	github.com/gin-gonic/gin v1.7.2 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/joho/godotenv v1.3.0
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
--- main.go
+++ main.go
@@ -9,35 +9,54 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/99designs/gqlgen/handler"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 )
 
 const defaultPort = "5000"
 
+// Defining the Graphql handler
+func graphqlHandler(db *gorm.DB) gin.HandlerFunc {
+	h := handler.GraphQL(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: db}}))
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func playgroundHandler() gin.HandlerFunc {
+	h := handler.Playground("GraphQL", "/query")
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
 func main() {
+	// 環境変数読み込み
 	err := godotenv.Load(fmt.Sprintf("./.%s.env", os.Getenv("GO_ENV")))
 	if err != nil {
 		return
 	}
-
+	// ポート番号設定
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
+	// gorm(DBORマッパー)設定
 	db, err := external.ConnectDatabase()
 	if err != nil {
 		panic(err)
 	}
 
-	srv := handler.NewDefaultServer(
-		generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: db}}),
-	)
-
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	// ginルーティング設定（gqlgen構成追加）
+	r := gin.Default()
+	r.GET("/", playgroundHandler())
+	r.POST("/query", graphqlHandler(db))
+	r.Run()
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
